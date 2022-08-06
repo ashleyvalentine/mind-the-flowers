e082a39bcc350b30002c7204a0e667afb103a862
Ashley Valentine <avalentinexo@gmail.com>
2022-08-06 09:03:03 -0400

    added css styling & method for rendering game in DOM

--- js/board.js
+++ js/board.js
@@ -8,42 +8,43 @@ const gameParameters = {
 
 class Board {
   constructor() {
+    //constructor parameters for future width and flowerAmount to allow user to change difficulty
     this.width = 10
     this.boardSpaces = this.width ** 2
     this.flowerAmount = 20
+    this.validSpacesAmount = this.boardSpaces - this.flowerAmount
+    this.flowerSpaces = this.createSpaces('flower', this.flowerAmount)
+    this.validSpaces = this.createSpaces('valid', this.validSpacesAmount)
   }
 
-  //   createSpaces(spaceType) {
-  //     for (i = 0; i < this.boardSpaces; )
-  //   } thinking about refactoring by using a for loop
-
-  createFlowerSpacesArray() {
-    const flowerAmount = 20
-    const flowerSpaces = Array(flowerAmount).fill('flower')
-    return flowerSpaces
+  createSpaces(spaceType, length) {
+    //spaceType == 'flower' || 'valid; length == this.flowerAmount || this.validSpacesAmount
+    let spaces = []
+    for (let i = 0; i < length; i++) {
+      spaces.push(spaceType)
+    }
+    return spaces
   }
 
-  createValidSpacesArray() {
-    const validSpaces = Array(this.boardSpaces - this.flowerAmount).fill(
-      'valid'
-    )
-    return validSpaces
+  createRandomizedSpacesArray() {
+    const randomizedSpaces = this.flowerSpaces
+      .concat(this.validSpaces)
+      .sort(() => Math.random() - 0.5)
+    return randomizedSpaces
   }
 
-  createGameArray() {
-    const gameArray = this.createValidSpacesArray().concat(
-      this.createFlowerSpacesArray()
-    )
-    return gameArray
-  }
-
-  createRandomizedGameSpacesArray() {
-    const randomizedGameSpaces = this.createGameArray().sort(
-      () => Math.random() - 0.5
-    )
-    return randomizedGameSpaces
+  renderBoardSpaces() {
+    const grid = document.querySelector('.grid')
+    const randomizedSpaces = this.createRandomizedSpacesArray()
+    for (let i = 0; i < this.boardSpaces; i++) {
+      const square = document.createElement('div')
+      square.setAttribute('id', i)
+      square.classList.add(randomizedSpaces[i])
+      grid.appendChild(square)
+    }
   }
 }
 
 let game = new Board()
-console.log(game.createRandomizedGameSpacesArray())
+game.renderBoardSpaces()
+console.log(game.createRandomizedSpacesArray())
